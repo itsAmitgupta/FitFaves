109ffb300ad4703aa54c27bf7a8174f3cda04bfc
Amit Gupta <guptaamitgupta819@gmail.com>
2024-03-30 21:59:49 +0530

    30/03

---
+++ Data.js
@@ -0,0 +1,36 @@
+const GymData = [
+        {
+          "id": 1,
+          "gymName": "FitZone Gym",
+          "address": "Shop No. 5, Narayan Chamber, Opp. Hotel Laxmi Palace, Vapi Main Road",
+          "city": "Vapi",
+          "activitiesAvailable": ["Cardio", "Weightlifting", "Yoga", "Zumba"],
+          "rating": 4.5
+        },
+        {
+          "id": 2,
+          "gymName": "PowerFit Gym",
+          "address": "Opp. Mahavir Hospital, Near Gunjan Theater, Vapi Town",
+          "city": "Vapi",
+          "activitiesAvailable": ["CrossFit", "Bodybuilding", "Aerobics", "Kickboxing"],
+          "rating": 4.2
+        },
+        {
+          "id": 3,
+          "gymName": "Fitness Edge",
+          "address": "3rd Floor, Shreenath Complex, Near Daman Ganga Bridge, GIDC",
+          "city": "Vapi",
+          "activitiesAvailable": ["Pilates", "Functional Training", "Spinning", "Martial Arts"],
+          "rating": 4.7
+        },
+        {
+          "id": 4,
+          "gymName": "Bodyline Fitness Center",
+          "address": "Shop No. 6-7, Saikrupa Complex, Opp. IDBI Bank, Char Rasta",
+          "city": "Vapi",
+          "activitiesAvailable": ["Circuit Training", "TRX", "Swimming", "Piloxing"],
+          "rating": 4.3
+        }
+]
+
+export {GymData}
--- src/Pages/CardSection.jsx
+++ src/Pages/CardSection.jsx
@@ -6,6 +6,11 @@ function CardSection() {
   return (
     <div>
         <p id="card" className='text-center font-bold text-gray-800 text-3xl mt-3'>Trending Fitness Centers Near You</p>
+        <div className='flex md:justify-end justify-center items-center h-[50px]'>
+          <div className='flex relative w-[300px] border h-[30px] border-black rounded-lg'>
+          <input type="search" name="" id="" className='px-1 border-none rounded-lg w-[200px]'/> <label className='bg-indigo-500 text-white text-center font-semibold text-xl transition-all hover:bg-indigo-600  absolute rounded-lg w-[100px] right-0' htmlFor="">Search</label>
+          </div>
+          </div>
         <Card/>
         <div className='w-full justify-center items-center flex my-3'>
         <div className='flex relative '>
